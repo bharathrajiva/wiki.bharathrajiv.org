1092ba81f189a51899faeb7297696da7cb07cae7
Bharath Rajiv A <mrenki19@gmail.com>
2025-03-23 11:31:58 +0530

    fix(creds) changes domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wiki.bharathrajiv.org',
-	// base: 'wiki.bharathrajiv.org',
 	integrations: [
 		starlight({
 			title: 'My Docs',
