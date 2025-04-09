f907f6c339dd757423708f1c34e9a95a86b4df58
Bharath Rajiv A <bharath.rajiv@seclore.com>
2025-04-09 10:51:31 +0000

    chore(logo): adds site logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { ion } from "starlight-ion-theme";
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://wiki.bharathrajiv.org',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Wiki',
+			description: "A wiki from the mind of Bharath Rajiv A",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/bharathrajiva',
 			},
+			logo: {
+				light: './src/assets/dark-site-logo.png',
+				dark: './src/assets/light-site-logo.png',
+				replacesTitle: true, // Hide the site title
+			  },
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/styles.css',
+			  ],
+			  plugins: [
+				ion()
+			  ],
 			sidebar: [
 				{
 					label: 'Guides',
